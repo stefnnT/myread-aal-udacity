c21473bc530797db26319a9befc2c3711687740c
Stephen SOLOMON <stephensolomon48@gmail.com>
2021-01-07 12:54:21 +0100

    refactor: Make Book a functional component

--- src/Book.js
+++ src/Book.js
@@ -1,53 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Book extends Component {
-  shelfChange = e => {
-    const { book, handleShelfChange } = this.props;
+const Book = props => {
+  const { book } = props;
+  const { title, authors, imageLinks, shelf } = book;
+
+  const shelfChange = e => {
+    const { book, handleShelfChange } = props;
     handleShelfChange(book, e.target.value);
   };
 
-  render() {
-    const { book } = this.props;
-    const { title, authors, imageLinks, shelf } = book;
-    if (!book.imageLinks) return null;
+  if (!book.imageLinks) return null;
 
-    return (
-      // <li>{JSON.stringify(book)}</li>
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${imageLinks.thumbnail})`
-              }}
-            />
-            <div className="book-shelf-changer">
-              <select
-                value={shelf}
-                onChange={this.shelfChange}
-                onBlur={this.shelfChange}
-              >
-                <option value="move" disabled>
-                  Move to...
-                </option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{title}</div>
-          <div className="book-authors">
-            {authors ? authors.join(", ") : ""}
+  return (
+    // <li>{JSON.stringify(book)}</li>
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${imageLinks.thumbnail})`
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select value={shelf} onChange={shelfChange}>
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
         </div>
-      </li>
-    );
-  }
-}
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors ? authors.join(", ") : ""}</div>
+      </div>
+    </li>
+  );
+};
 
 export default Book;
